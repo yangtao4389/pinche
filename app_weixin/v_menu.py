698e7a2bb9e0d7f0a8df244a821de7bed4371f8c
yangtao <438936731@qq.com>
2019-05-28 18:14:18 +0800

    修改菜单及图

--- app_weixin/v_menu.py
+++ app_weixin/v_menu.py
@@ -7,35 +7,36 @@ def createMenu(request):
     myrobotclient.create_menu({
         "button": [
             {
-                "type": "click",
-                "name": "我要拼车",
-                "key": "key1"
-            },
-            {
                 "type": "view",
-                "name": "拼车首页",
+                "name": "我要拼车",
                 "url": "http://www.rhax.cn/WebApp/Home"
             },
             {
-                "name": "我的",
+                "type": "view",
+                "name": "发布行程",
+                "url": "http://www.rhax.cn/WebApp/UserAss/Publish"
+            },
+            {
+                "name": "个人中心",
                 "sub_button": [
+                    {
+                        "type": "view",
+                        "name": "个人中心",
+                        "url": "http://www.rhax.cn/WebApp/UserCenter"
+                    },
                     {
                         "type": "view",  # web页面
                         "name": "更新菜单",
                         "url": "http://211.149.180.119/wxbackend/createMenu/"
                     },
-                    {
-                        "type": "miniprogram",  # 小程序
-                        "name": "万年历",
-                        "url": "http://mp.weixin.qq.com",
-                        "appid": "wx286b93c14bbf93aa",
-                        "pagepath": "pages/lunar/index"
-                    },
-                    {
-                        "type": "click",
-                        "name": "赞一下我们",
-                        "key": "V1001_GOOD"
-                    }]
+                    # {
+                    #     "type": "miniprogram",  # 小程序
+                    #     "name": "万年历",
+                    #     "url": "http://mp.weixin.qq.com",
+                    #     "appid": "wx286b93c14bbf93aa",
+                    #     "pagepath": "pages/lunar/index"
+                    # },
+                   ]
             }]
     })
     return HttpResponse("ok")
