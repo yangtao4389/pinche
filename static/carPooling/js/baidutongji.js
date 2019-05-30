8b8c1d5e9f60db0da6841cd4c5e97864813a1ac6
yangtao <438936731@qq.com>
2019-05-30 16:51:49 +0800

    百度统计

---
+++ static/carPooling/js/baidutongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f25df7971fc376d5919f03cd6c411079";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+
